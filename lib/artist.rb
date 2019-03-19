eb0025984786f757be5a3cdfd862a00e6bd65f35
Eyal Toledano <eyaltoledano@me.com>
2019-03-19 19:11:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,7 +10,6 @@ class Artist
   end
 
   def add_song(song)
-    binding.pry
     the_song = song
     the_song.artist = self
     @songs << the_song
@@ -21,6 +20,7 @@ class Artist
   end
 
   def add_song_by_name(name)
+    binding.pry
     song = add_song(name)
   end
 
